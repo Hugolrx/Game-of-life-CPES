6b3838e5dade179a97108897e45bfe7a7b58a32f
Hugolrx <73792070+Hugolrx@users.noreply.github.com>
2020-12-17 21:46:09 +0100

    Update game_of _life.py

--- game_of _life.py
+++ game_of _life.py
@@ -17,6 +17,7 @@ def change_col(bouton) :
     else :
         bouton.configure(bg="white", activebackground="black")
 
+
 def boutons() :
     global dico
 
@@ -27,15 +28,20 @@ def boutons() :
             dico[f"b{i}{j}"].configure(command=partial(change_col, dico[f"b{i}{j}"]))
             dico[f"b{i}{j}"].grid(row=i, column=j)
 
-    launch = Button(root, command(generation), text="  Go !  ")
+    launch = Button(root, text="  Go !  ")
+    launch.configure(command=generation)
     launch.grid(row=5, column=50)
 
 
 
-
-
-def generation() :
+def generation(grid = None) :
     global dico
+
+    if grid == None :
+        for i in range(10) :
+            for j in range(10) :
+                bg_color, active_bg_color = dico[f"b{i}{j}"].cget("bg"), dico[f"b{i}{j}"].cget("activebackground")
+
 
     for i in range(len(grid)) :
         for j in range(len(grid[0])) :
