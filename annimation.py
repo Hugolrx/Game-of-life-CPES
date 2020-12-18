8cd2bee80bc2dc140b2d08ef2d553e005162988a
Hugolrx <73792070+Hugolrx@users.noreply.github.com>
2020-12-18 13:35:22 +0100

    Update annimation.py

--- annimation.py
+++ annimation.py
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 from tkinter import *
 from functools import partial
 from time import sleep
@@ -24,43 +18,57 @@ def change_col(bouton) :
         bouton.configure(bg="white", activebackground="black")
 
 
-def boutons() :
+def buttons() :
     global dico
 
-    for i in range(10) :
-        for j in range(10) :
+    for i in range(30) :
+        for j in range(30) :
 
-            dico[f"b{i}{j}"] = Button(root, padx=11, bg="white", activebackground="black", bd=1)
+            dico[f"b{i}{j}"] = Button(root, padx=12, bg="white", activebackground="black")
             dico[f"b{i}{j}"].configure(command=partial(change_col, dico[f"b{i}{j}"]))
             dico[f"b{i}{j}"].grid(row=i, column=j)
 
-    launch = Button(root, text="  Go !  ")
-    launch.configure(command=generation)
-    launch.grid(row=5, column=50)
+    dico["e"] = Entry(root, width = 30)
+    dico["e"].grid(row=4, column=50)
+
+    dico["launch"] = Button(root, text="  Go !  ")
+    dico["launch"].configure(command=grid_generation)
+    dico["launch"].grid(row=5, column=50)
+
+def grid_generation() :
+    # if e.get() != "" :
+    #     pass #appeler la fonciton de Coco pour faire la grille à partir du fichier
+    # # else :
+    grid = []
+    for i in range(30) :
+        grid.append([])
+        for j in range(30) :
+            if dico[f"b{i}{j}"].cget("bg") == "white" :
+                grid[i].append(0)
+            else :
+                grid[i].append(1)
+
+    dico["e"].destroy()
+    dico["launch"].destroy()
+
+    generation_plateau(grid)
 
 
-
-def generation(grid = None) :
+def generation_plateau(grid) :
     global dico
 
-    if grid == None :
-        for i in range(10) :
-            for j in range(10) :
-                bg_color, active_bg_color = dico[f"b{i}{j}"].cget("bg"), dico[f"b{i}{j}"].cget("activebackground")
-
-
-    for i in range(len(grid)) :
-        for j in range(len(grid[0])) :
+    for i in range(30) :
+        for j in range(30) :
 
             if grid[i][j] == 0 :
                 color = "white"
             else :
                 color = "black"
 
-            dico[f"b{i}{j}"] = Label(root, padx=11, bg=color)
-
+            dico[f"b{i}{j}"].destroy()
+            dico[f"b{i}{j}"] = Label(root, padx=15, pady=5, bg=color, relief=RAISED)
             dico[f"b{i}{j}"].grid(row=i, column=j)
-
+#
 
 
 def update(grid) :
@@ -79,7 +87,7 @@ def update(grid) :
 
 dico = {}
 
-boutons()
+buttons()
 
 # generation([[0, 0, 0, 0, 0, 0, 0, 0],
 #             [0, 0, 0, 0, 0, 0, 0, 0],
