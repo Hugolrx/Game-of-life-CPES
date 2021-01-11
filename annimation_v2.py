313c174898dfb7b08073278ac32f874ea7358b92
Hugolrx <73792070+Hugolrx@users.noreply.github.com>
2021-01-11 22:49:51 +0100

    Update annimation_v2.py

--- annimation_v2.py
+++ annimation_v2.py
@@ -5,26 +5,35 @@ from time import sleep
 root = Tk()
 root.title("Le Jeu de la Vie")
 
-# Hola
+# A faire : changer la taille de la grille ;
+#           détecter la taille du fichier lu (ou en faire une par défaut, et dans ce cas, centrer les coordonnées) ;
+#           --> faire avancer étape par étape
+#           bouton de réinitialisation
 
-taille = 25
+taille = 30
+pixel = 1000
 
+c = Canvas(root, height = pixel, width = pixel, bg="grey")
+c.grid(row=0, column=0, rowspan = 20)
+side = pixel/taille
 
-def change_col(bouton) :        # changement de couleur des boutons
-    if bouton.cget("bg") == "white" :
-        bouton.configure(bg="black", activebackground="white")
+grid_display = []
+
+def change_col(coord, _) :        # changement de couleur des boutons
+    i,j = coord
+    if c.itemcget(grid_display[i][j], "fill") == "white" :
+        c.itemconfig(grid_display[i][j], fill="black")
     else :
-        bouton.configure(bg="white", activebackground="black")
+        c.itemconfig(grid_display[i][j], fill="white")
 
 
-def buttons() :             # implémentation des boutons pour initialiser la grille et les contrôles
+def boutons() :             # implémentation des boutons pour initialiser la grille et les contrôles
 
-    for i in range(25) :
+    for i in range(taille) :
         grid_display.append([])
-        for j in range(25) :
-            grid_display[i].append(Button(root, padx=12, bg="white", activebackground="black"))
-            grid_display[i][j].configure(command=partial(change_col, grid_display[i][j]))
-            grid_display[i][j].grid(row=i, column=j)
+        for j in range(taille) :
+            grid_display[i].append(c.create_rectangle(side*j, side*i, side*(j+1), side*(i+1), width=2, fill="white", tags=f"{i}_{j}"))
+            c.tag_bind(f"{i}_{j}","<Button-1>", partial(change_col, (i,j)))
 
     global entry_file, launch_button, speed_scale, step, step_label, save_button, save_entry
 
@@ -37,7 +46,7 @@ def buttons() :             # implémentation des boutons pour initialiser la gr
     launch_button.grid(row=5, column=50)
 
     # Curseur de la vitesse
-    speed_scale = Scale(root, orient="horizontal", from_ = 0, to = 20, resolution=0.1, tickinterval=2, label="Speed (steps/sec)", length = 330)
+    speed_scale = Scale(root, orient="horizontal", from_ = 0, to = 16, resolution=0.1, tickinterval=2, label="Speed (steps/sec)", length = 330)
     speed_scale.set(2)
     speed_scale.grid(row=7, column=50, rowspan=3)
 
@@ -63,13 +72,17 @@ def sauvegarde() :
     f = open(f"{file_name}.txt", "w")
     for i in range(len(grid_display)) :
         for j in range(len(grid_display)) :
-            if grid_display[i][j].cget("bg") == "black" :
+            if c.itemcget(grid_display[i][j], "fill") == "black" :
                 f.write(f"{i} {j}\n")
     f.close
 
 
 
 def grid_generation() :                 # création de la première grille 0_1
+
+    for i in range(len(grid_display)) :         # désactivation des cases
+        for j in range(len(grid_display)) :
+            c.dtag(grid_display[i][j], f"{i}_{j}")
 
     if entry_file.get() != "" :
         file = open(f"{entry_file.get()}.txt", "r")
@@ -86,7 +99,7 @@ def grid_generation() :                 # création de la première grille 0_1
         for i in range(len(grid_display)) :
             grid.append([])
             for j in range(len(grid_display)) :
-                if grid_display[i][j].cget("bg") == "white" :
+                if c.itemcget(grid_display[i][j], "fill") == "white" :
                     grid[i].append(0)
                 else :
                     grid[i].append(1)
@@ -109,42 +122,10 @@ def grid_generation() :                 # création de la première grille 0_1
                 first_grid[x+1][y+1] += 2
 
 #boucle d'initialisation à partir de la grille racine
-
-
-    generation_plateau(first_grid)
-
-
-def generation_plateau(grid) :    # destruction des boutons et remplacement par des Labels à partir de la grille 0_1    (du coup non)
-
     entry_file.destroy()
     launch_button.destroy()
     save_button.destroy()
     save_entry.destroy()
-
-            # IMPORTANT J'ai gardé les boutons en les désactivant et on gagne beaucoup en rapidité
-
-    # for i in range(30) :
-    #     for j in range(30) :
-    #         grid_display[i][j].destroy()
-
-    pixel = 900
-    side = pixel/len(grid)
-    global c
-    c = Canvas(root, height = pixel, width = pixel, bg="grey")
-    c.grid(row=0, column=0)
-
-    for i in range(len(grid)) :
-        for j in range(len(grid)) :
-
-            grid_display[i][j].destroy()
-
-            if grid[i][j] % 2 == 0 :
-                color = "white"
-            else :
-                color = "black"
-
-            grid_display[i][j] = c.create_rectangle(side*j, side*i, side*(j+1), side*(i+1), width=2, fill=color)
-
 
 
     global pause_button
@@ -152,7 +133,43 @@ def generation_plateau(grid) :    # destruction des boutons et remplacement par 
     pause_button = Button(root, text="Pause", command=partial(pause, grid))
     pause_button.grid(row=0, column = 50)
 
-    update(grid)
+    update(first_grid)
+
+    # generation_plateau(first_grid)
+
+
+def generation_plateau(grid) :    # destruction des boutons et remplacement par des Labels à partir de la grille 0_1    (du coup non)
+    pass
+    # entry_file.destroy()
+    # launch_button.destroy()
+    # save_button.destroy()
+    # save_entry.destroy()
+
+#             # IMPORTANT J'ai gardé les boutons en les désactivant et on gagne beaucoup en rapidité
+#
+#     # for i in range(30) :
+#     #     for j in range(30) :
+#     #         grid_display[i][j].destroy()
+#
+#
+#     for i in range(len(grid)) :
+#         for j in range(len(grid)) :
+#
+#             if grid[i][j] % 2 == 0 :
+#                 color = "white"
+#             else :
+#                 color = "black"
+#
+#             # grid_display[i][j] = c.create_rectangle(side*j, side*i, side*(j+1), side*(i+1), width=2, fill=color)
+
+
+
+    # global pause_button
+    # # Bouton de pause
+    # pause_button = Button(root, text="Pause", command=partial(pause, grid))
+    # pause_button.grid(row=0, column = 50)
+    #
+    # update(grid)
 
 
 
@@ -216,7 +233,6 @@ def resume(grid) :
 
 
 
-grid_display = []
 
 
 buttons()
